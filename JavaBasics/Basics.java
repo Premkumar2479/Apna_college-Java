cffd1b938740facc02fd24775a3ef252b4049616
Premkumar2479 <pk2335301@gmail.com>
2026-02-11 01:24:15 +0530

    update java

--- JavaBasics/Basics.java
+++ JavaBasics/Basics.java
@@ -2,7 +2,10 @@ package JavaBasics;
 
 public class Basics {
     public static void main (String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World!\n");
+        System.out.print("Hello Nick\n");
+        System.out.println("Hello Nick");
+
     }
 
 }
---
+++ JavaBasics/Pattern.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Pattern {
+    public static void main(String[] args){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
