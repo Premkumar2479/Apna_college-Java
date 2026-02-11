1aa72871118182f283f317ce3ceaaa5f53223567
Premkumar2479 <pk2335301@gmail.com>
2026-02-11 09:13:53 +0530

    update pages

---
+++ Variables/Simplevalues.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Simplevalues {
+    public static void main(String[] args){
+        int a =34;
+        int b =45;
+        String name = "Tony Stark";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        a =56;
+        System.out.println(a);
+
+        char c = 'h';
+        System.out.println(c);
+}
+}
