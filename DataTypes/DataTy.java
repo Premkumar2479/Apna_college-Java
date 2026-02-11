e4eeef57bb62d157647c554c9192dba8d12f7fd7
Premkumar2479 <pk2335301@gmail.com>
2026-02-11 10:48:06 +0530

    add files

---
+++ DataTypes/DataTy.java
@@ -0,0 +1,20 @@
+package DataTypes;
+
+public class DataTy {
+    public static void main(String[] args){
+        //long
+        //double
+        byte b = 8;
+        char ch ='a';
+        boolean var = false;
+        float price = 10.9f;
+        int number =45;
+        short n =230;
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(var);
+        System.out.println(price);
+        System.out.println(number);
+        System.out.println(n);
+    }
+}
