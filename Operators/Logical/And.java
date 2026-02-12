e919d7ae304187cbf4231a19351f576ccb229e91
Premkumar2479 <pk2335301@gmail.com>
2026-02-12 14:48:39 +0530

    add updates

---
+++ Operators/Logical/And.java
@@ -0,0 +1,9 @@
+package Operators.Logical;
+
+public class And {
+    public static void main(String[] args){
+        System.out.println( (3>2) && (5>0));
+        System.out.println( (4<3) || (6<5));
+        System.out.println( !(9>2) );
+    }
+}
