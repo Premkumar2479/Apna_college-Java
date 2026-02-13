f3caeddc58cda1a634eb5afc2c6833dd00583e4a
Premkumar2479 <pk2335301@gmail.com>
2026-02-13 23:54:31 +0530

    some updates

--- Conditionalstatem/Switch.java
+++ Conditionalstatem/Switch.java
@@ -1,9 +1,31 @@
 package Conditionalstatem;
 
+import java.util.Scanner;
+
 public class Switch {
     public static void main(String[] args){
-        int 
+        Scanner sc = new Scanner(System.in);
 
-        
+        int weeks = sc.nextInt();
+
+        switch(weeks){
+            case 1 : System.out.println("Monday");
+            break; 
+            case 2 : System.out.println("Tuesday");
+            break;
+            case 3 : System.out.println("Webnesday");
+            break;
+            case 4 : System.out.println("Thrusday");
+            break;
+            case 5 : System.out.println("Friday");
+            break;
+            case 6 : System.out.println("Saturday");
+            break;
+            case 7 : System.out.println("Sunday");
+            break;
+            default : System.out.println("Not Weeks");
+        }
+        sc.close();
+
     }
 }
