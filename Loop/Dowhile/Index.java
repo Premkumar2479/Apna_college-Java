754de73cfcdce92f379870a61471ce6295cb9ae8
Premkumar2479 <pk2335301@gmail.com>
2026-02-16 14:45:35 +0530

    some updates

--- Loop/Dowhile/Index.java
+++ Loop/Dowhile/Index.java
@@ -1,5 +1,11 @@
 package Loop.Dowhile;
 
 public class Index {
-    
+    public static void main(String[] args){
+        int counter= 1;
+        do{
+            System.out.println("Hello world");
+            counter++;
+        }while(counter <=10);
+    }
 }
