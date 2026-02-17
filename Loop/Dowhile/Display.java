06232c7d52c9d8ce9cc37fb5c6ff783162d70f55
Premkumar2479 <pk2335301@gmail.com>
2026-02-17 22:32:05 +0530

    some text

--- Loop/Dowhile/Display.java
+++ Loop/Dowhile/Display.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Display {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int n;
+
         do {
             System.out.println("Enter your number :");
-            int n = sc.nextInt();
+            n = sc.nextInt();
 
-            if(n% 10 == 0){
-                continue;
+            if(n % 10 == 0){
+                System.out.println("Multiple of 10 entered. Loop stopped.");
+                break;
             }
-            System.out.println("Number was : " + n);
-        }while(true);
 
-        // sc.close();
+            System.out.println("Number was : " + n);
+
+        } while(true);
+
+        sc.close();
     }
 }
