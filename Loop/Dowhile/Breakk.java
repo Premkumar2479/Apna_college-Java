a005ba1c3346a27671d242c463b45aea82a67333
Premkumar2479 <pk2335301@gmail.com>
2026-02-17 13:55:18 +0530

    some text

---
+++ Loop/Dowhile/Breakk.java
@@ -0,0 +1,12 @@
+package Loop.Dowhile;
+
+public class Breakk {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            if( i == 3){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ Loop/Dowhile/Checkno.java
@@ -0,0 +1,21 @@
+package Loop.Dowhile;
+
+import java.util.Scanner;
+
+public class Checkno {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.print("Enter your namber : ");
+            int n = sc.nextInt();
+
+
+            if(n % 10 == 0){
+                break;
+            }
+            System.out.println(n);
+        }while(true);
+        sc.close();
+    }
+}
