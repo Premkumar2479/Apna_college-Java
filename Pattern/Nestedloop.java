c360f67c7616137de41f71c8dbb7f20ceddf3255
Premkumar2479 <pk2335301@gmail.com>
2026-02-17 22:37:53 +0530

    some text

---
+++ Pattern/Nestedloop.java
@@ -0,0 +1,9 @@
+package Pattern;
+
+public class Nestedloop {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            System.out.println("*");
+        }
+    }
+}
