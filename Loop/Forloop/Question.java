4cb24d03290a854c65e248b6085283c9b4287386
Premkumar2479 <pk2335301@gmail.com>
2026-02-25 23:54:07 +0530

    some updates

---
+++ Loop/Forloop/Question.java
@@ -0,0 +1,14 @@
+package Loop.Forloop;
+
+import java.util.Scanner;
+
+public class Question {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+            sc.close();
+        
+    }
+}
