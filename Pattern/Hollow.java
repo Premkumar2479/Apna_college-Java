864685780c5072a6038a84662508cd848286a47c
Premkumar2479 <pk2335301@gmail.com>
2026-02-28 00:26:57 +0530

    aome updates

--- Pattern/Hollow.java
+++ Pattern/Hollow.java
@@ -1,9 +1,23 @@
 package Pattern;
 
 public class Hollow {
-    public static void main(String[] args){
-        for(int i=1; i<=4; i++){
-            System.out.println("*****");
+    public static void hollow_rectangle(int totRows, int totCols){
+        for(int i=1; i<=totRows; i++){
+            //inner -columns
+            for(int j=1; j<= totCols; j++){
+                //cell - (i,j)
+                if(i == 1 || i == totRows || j == 1 || j == totCols){
+                   System.out.print("*"); 
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
+    }
+
+
+    public static void main(String[] args){
+        hollow_rectangle(4,5);
     }
 }
