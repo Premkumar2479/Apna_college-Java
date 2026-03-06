93218c48e7891308370650bd4da9c7cab32a753e
Premkumar2479 <pk2335301@gmail.com>
2026-03-06 21:47:57 +0530

    some updates

---
+++ Conditionalstatem/Firstcode.java
@@ -0,0 +1,21 @@
+package Conditionalstatem;
+
+import java.util.Scanner;
+
+public class Firstcode {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T =sc.nextInt();
+        while(T-->0){
+            int N =sc.nextInt();
+            int X =sc.nextInt();
+
+            if(N<=X){
+                System.out.println("yes");
+            }else{
+                System.out.println("no");
+            }
+        }
+        sc.close();
+    }
+}
