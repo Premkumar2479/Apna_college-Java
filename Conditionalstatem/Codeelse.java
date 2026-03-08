2703bc3d4192c25ebd91a17be43f600bd81dce60
Premkumar2479 <pk2335301@gmail.com>
2026-03-08 23:22:51 +0530

    updats files

---
+++ Conditionalstatem/Codeelse.java
@@ -0,0 +1,12 @@
+package Conditionalstatem;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(31 * X);
+        sc.close();
+    }
+}
