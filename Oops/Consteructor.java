85a587a7db5e3f1570688c0cd1a456b9a49d0e85
Premkumar2479 <pk2335301@gmail.com>
2026-03-14 23:20:00 +0530

    some updates

---
+++ Oops/Consteructor.java
@@ -0,0 +1,18 @@
+package Oops;
+
+
+public class Consteructor {
+    public static void main(String[] args){
+        Student s1 = new Student("Nick");
+        System.out.println(s1.name);
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+
+    Student(String name){
+        this.name =name;
+    }
+}
