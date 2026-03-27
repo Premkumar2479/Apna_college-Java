bd7630e8baa5a0ca21b98eabccf018def30b19b8
Premkumar2479 <pk2335301@gmail.com>
2026-03-27 22:40:23 +0530

    some updaates

--- Loop/Forloop/Table.java
+++ Loop/Forloop/Table.java
@@ -12,12 +12,7 @@ public class Table {
 
         for(int i=1; i<=10; i++){
              System.out.println(N + " x " + i + " = " + (N * i));
-
         }
-
-        
-
-
         sc.close();
     }
 }
