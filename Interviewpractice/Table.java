80ed312aa552cecfe28ae6947f750114ab93cee9
Premkumar2479 <pk2335301@gmail.com>
2026-04-09 23:48:31 +0530

    some updates

---
+++ Interviewpractice/Table.java
@@ -0,0 +1,14 @@
+package Interviewpractice;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=1; i>1; i++){
+            System.out.println(N + " x " + i + " = " + (N * i) );
+        }
+            sc.close();
+    }
+}
